ad6d154cbc4e34a13cbfdcf2394fd6a0632b187a
João Melo <devjoaomelo@gmail.com>
2024-12-09 16:39:58 -0300

    done

---
+++ 1_TwoSum/Solution.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Text;
+
+namespace LeetCodeChallenges._1_TwoSum;
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> hash = new Dictionary<int,int>();
+        for (int i = 0; i < nums.Length; i++){
+            int append = target - nums[i];
+            if(hash.ContainsKey(append)){
+                return new int[]{hash[append], i};
+            }
+
+            hash[nums[i]] = i;
+        }
+        return new int[]{ -1, -1};   
+    }
+}
