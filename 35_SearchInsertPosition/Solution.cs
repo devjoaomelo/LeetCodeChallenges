605ba1cb11306293eb7e2d0c2b9d8316d37d02c7
João Melo <devjoaomelo@gmail.com>
2024-12-11 21:50:21 -0300

    done

---
+++ 35_SearchInsertPosition/Solution.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace LeetCodeChallenges._35_SearchInsertPosition;
+
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        // recebe o index ou retorna -1
+        int idx = Array.IndexOf(nums, target);
+        if(idx != -1){
+            // se for diferente de -1 achou
+            return idx;
+        }
+        // se for maior que o ultimo elemento retorna como o proximo elemento
+        if(target > nums[nums.Length-1]){
+            return nums.Length;
+
+        }
+        // percorrendo para ver se é maior que o elemento anterior
+        for(int i = nums.Length - 1; i >= 0; i--){
+            if(nums[i] < target){
+                return i + 1;
+            }
+            
+        }
+        return 0;
+    }
+}
