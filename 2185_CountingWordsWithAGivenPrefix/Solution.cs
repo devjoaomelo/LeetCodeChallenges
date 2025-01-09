cd10a4f2badc2460c91536288dce365512317532
João Melo <devjoaomelo@gmail.com>
2025-01-09 20:29:22 -0300

    done

---
+++ 2185_CountingWordsWithAGivenPrefix/Solution.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace LeetCodeChallenges._2185_CountingWordsWithAGivenPrefix;
+
+public class Solution {
+    public int PrefixCount(string[] words, string pref) {
+        int count = 0;
+        for(int i = 0; i < words.Length; i++){
+            if(words[i].StartsWith(pref)){
+                count++;
+            }
+        }
+
+        return count;
+        
+    }
+}
